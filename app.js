ad6fa93967225c6460a3f129d6f7823969fd0585
Spb-B <135686449+Spb-B@users.noreply.github.com>
2023-10-28 14:18:01 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+      });
+
+})
